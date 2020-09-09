c1d6e5b4d1107fe3f62ddbd8511e5428369d3f8a
tuanphat0608 <70989048+tuanphat0608@users.noreply.github.com>
2020-09-09 08:46:18 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,116 @@
+<!doctype html>
+<html lang="en">
+<link rel="stylesheet" href="style.css">
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+		integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link href="https://fonts.googleapis.com/css?family=Lobster&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Lobster+Two&display=swap" rel="stylesheet">
+
+	<link rel="stylesheet" href="css/style.css">
+
+	<title>Kata Code Camp</title>
+</head>
+
+<body>
+	<div id="top"></div>
+	<div class="navbar navbar-expand-sm navbar-light bg-light fixed-top">
+		<a class="navbar-brand"  href="index.php"></a>
+		<button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId"
+			aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="collapsibleNavId">
+			<ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+				<li class="logo">
+					<a href="index.php"><img src="logo1.png"></a>
+				</li>
+				<li class="nav-item active">
+					<a class="nav-link" href="index.php">Home<span
+							class="sr-only">(current)</span></a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link">Frontend</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link">Backend</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link">Contact</a>
+				</li>
+			</ul>
+			<a href="">Login</a>
+			<a href="">Create Account</a>
+		</div>
+	</div>
+
+	<div class="jumbotron" id="banner"></div>
+	</div>
+<div class="container1">
+  <div class="row">
+    <div class="col-md-8" id="colum1">
+	<form>
+  <div class="form-group">
+    <label for="Username">Username</label>
+    <input type="Username" class="form-control" id="Username" placeholder="Username">
+  </div>
+  <div class="form-group">
+    <label for="Password1">Password</label>
+    <input type="password" class="form-control" id="Password1" placeholder="Password">
+  </div>
+  <div class="form-group">
+    <label for="Password2">Reenter Password</label>
+    <input type="password" class="form-control" id="Password2" placeholder="Reenter Password">
+  </div>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Your name">
+  </div>
+  <button type="submit" class="btn btn-primary">Sign Up</button>
+</form>
+    </div>
+    <div class="col-md-4" id="colum2">
+	<img src="AdobeStock_63139827.jpeg">
+    </div>
+  </div>
+</div>
+<div class="container2-fluid">
+  <div class="row">
+    <div class="col-sm">
+	<div class="card" style="width: 18rem;">
+  		<img src="html5.png">
+  	<div class="card-body">
+    	<h5 class="card-title">Learn HTML</h5>
+   	 	<p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. At voluptatibus</p>
+  	</div>
+	</div>
+    </div>
+    <div class="col-sm">
+	<div class="card" style="width: 18rem;">
+  		<img src="js.png">
+  	<div class="card-body">
+    	<h5 class="card-title">Learn JS</h5>
+   	 	<p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. At voluptatibus</p>
+  	</div>
+	</div>
+    </div>
+    <div class="col-sm">
+	<div class="card" style="width: 18rem;">
+  		<img src="css.png">
+  	<div class="card-body">
+    	<h5 class="card-title">Learn CSS</h5>
+   	 	<p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. At voluptatibus</p>
+  	</div>
+	</div>
+    </div>
+  </div>
+</div>
+</body>
+
+</html>
